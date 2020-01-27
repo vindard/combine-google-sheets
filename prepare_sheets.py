50e28254d98dbc84781211c6c17e3dfa41d3e823
vindard <17693119+vindard@users.noreply.github.com>
2020-01-26 21:55:32 -0400

    refactor `run_compare` to return boolean and results

--- prepare_sheets.py
+++ prepare_sheets.py
@@ -163,33 +163,22 @@ def threaded_add_headers_to_sheet_dicts(sheet_dicts: SheetDicts) -> SheetDicts:
 
     return updated_sheet_dicts
 
-def compare_sheet_headers(sheet_dicts, base_wb_name, compare_wb_name, sheet_num=4):
+
+
+def fetch_sheet_headers(sheet_dicts, wb_name, sheet_num):
     sheet_idx = sheet_num - 1
     keys = list(sheet_dicts.keys())
 
     # Fetch ws1 dict for sheet_num
-    wb1_name = base_wb_name
-    wb1 = sheet_dicts[wb1_name]
-    wb1_names = list(wb1.keys())
+    wb = sheet_dicts[wb_name]
+    wb_names = list(wb.keys())
 
-    wb1_compare_sheet_name = wb1_names[sheet_idx]
-    wb1_compare_sheet = wb1[wb1_compare_sheet_name]
-    wb1_sheet_headers = wb1_compare_sheet['headers']
+    wb_compare_sheet_name = wb_names[sheet_idx]
+    wb_compare_sheet = wb[wb_compare_sheet_name]
+    wb_sheet_headers = wb_compare_sheet['headers']
 
-    # Fetch ws2 dict for sheet_num
-    wb2_name = compare_wb_name
-    wb2 = sheet_dicts[wb2_name]
-    wb2_names = list(wb2.keys())
+    return wb_sheet_headers
 
-    wb2_compare_sheet_name = wb2_names[sheet_idx]
-    wb2_compare_sheet = wb2[wb2_compare_sheet_name]
-    wb2_sheet_headers = wb2_compare_sheet['headers']
-
-    # Prepare results
-    wb1_headers_tuple = wb1_sheet_headers, set(wb1_sheet_headers)
-    wb2_headers_tuple = wb2_sheet_headers, set(wb2_sheet_headers)
-
-    return wb1_headers_tuple, wb2_headers_tuple
 
 def run():
     wbs_open = fetch_all_workbooks()
@@ -199,22 +188,43 @@ def run():
     return sheet_dicts_w_headers
 
 def run_compare(sheet_dicts, wb_idx=0):
+    compare_report, report_body = '', ''
     keys = list(sheet_dicts.keys())
     base_wb_name = MASTER_WORKBOOK or keys[0]
+    base_sheets_names = list(sheet_dicts[base_wb_name].keys())
     compare_wb_name = keys[wb_idx]
 
-    print("\n====================")
-    print(f"Running compare for workbooks:\n- '{base_wb_name}'\n- '{compare_wb_name}'")
-    for i in range(10):
-        (h1, hs1), (h2, hs2) = compare_sheet_headers(sheet_dicts, base_wb_name, compare_wb_name, sheet_num=i)
+    base_headers = {sheet_name: fetch_sheet_headers(sheet_dicts, base_wb_name, sheet_num=i)
+                        for i, sheet_name in enumerate(base_sheets_names)}
+
+    compare_report += "\n====================\n\n"
+    compare_report += f"Running compare for workbooks:\n- '{base_wb_name}'\n- '{compare_wb_name}'\n"
+    for i, sheet_name in enumerate(base_sheets_names):
+        h1 = base_headers[sheet_name]
+        h2 = fetch_sheet_headers(sheet_dicts, compare_wb_name, sheet_num=i)
+
+        if h1 == h2:
+            continue
+
+        report_body += f"\n\nSheet #{i+1}: {sheet_name}\n"
+        report_body += f"---\n"
+        hs1, hs2 = set(h1), set(h2)
         set_diff = hs1 - hs2
-        print(f"\n\n[ {i} ]")
-        if not set_diff:
-            print("No differences!")
-        else:
+        if set_diff:
             for i, val in enumerate(set_diff):
-                print(f"{i+1}. {val}")
+                if val in h1:
+                    report_body += f"{i+1}. Missing in compare sheet:\n  {val}\n"
+                else:
+                    report_body += f"{i+1}. Extra in compare sheet (not in base sheet):\n  {val}\n"
+        else:
+            report_body += f"Headers don't match against base sheet, please review.\n"
+
+    compare_report += report_body or "\nNo differences found!"
+
+    return bool(report_body), compare_report
 
 if __name__ == "__main__":
     sheet_dicts = run()
-    run_compare(sheet_dicts, 2)
+    diff, results = run_compare(sheet_dicts, 2)
+    if diff:
+        print(results)
